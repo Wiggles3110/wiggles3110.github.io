56458adae483d378f5616eb1c16d64a1d7c59e86
Will Nelson <nelsonwill254@gmail.com>
2025-08-11 10:46:08 -0700

    changed about me text and removed art gallery section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -10,18 +10,16 @@ function Home() {
             title: "About Me", 
             text: (
             <>
-            Howdy! I’m Will, a recent Computer Science graduate based in the sunny city of Phoenix, Arizona.
+            I’m a recent Computer Science graduate based in the sunny city of Phoenix, Arizona. In May 2025,
              I earned my Bachelor of Science in Computer Science, graduating summa cum laude from Arizona State 
-             University, along with an Associate’s degree in Mathematics and Science.
+             University. I also hold an Associate’s degree in Mathematics and Science from GLendale Community College.
             <br/><br/>
             From a young age, I have been passionate about creating. I enjoyed drawing, building with LEGO,
              and crafting from paper. That same creativity now fuels my love for programming and problem solving. 
              This website, built with React, is just one example of how I enjoy experimenting with code and bringing ideas to life.
              <br/><br/>
             I have hands-on experience with a variety of languages, including C++, Java, Python, and C#. Whether I am developing software,
-             designing interactive systems, or analyzing data, I approach every challenge with curiosity, focus, and a drive to keep learning.
-            <br/><br/>
-            Thanks for stopping by!
+             designing interactive systems or analyzing data, I approach every challenge with curiosity, focus and a drive to keep learning.
             </>
             ),
         },
@@ -37,7 +35,7 @@ function Home() {
               while contributing to a real-world outreach effort.
             <br/>
             <a href="https://missiontopsyche-iridium.github.io/iridium_22a_m-type_sim-asu/"> 
-            <u>PLAY IT HERE!</u></a>
+            <u>PLAY IT HERE</u></a>
             <br/><br/>
             React Web Project | Solo <br/>
             I designed and developed this website using React and styled it with custom CSS. Built with Vite, the site serves
@@ -59,8 +57,8 @@ function Home() {
             <> 
             In the near future, I plan to develop two games I have dreamed about making since childhood: a Metroidvania, 
             a genre focused on exploration and unlocking new areas through abilities, and a turn-based RPG, where strategy
-             and story take center stage. These projects will let me combine my art and programming skills, giving me full
-              creative control and serving as a long-term expression of my passion for game development and storytelling.
+             and story take center stage. These projects will give me the opportunity to combine my art and programming skills, 
+             and help fulfill my passion for game development and storytelling.
             </>
         ),
     },
@@ -73,6 +71,7 @@ function Home() {
             {cont.map((item, index) => (
             <div key={index}>
                 <Content info={item} />
+{/*                 
                 {item.title === "Hobbies" && (
                 <div className = "content-info">
                     <h3>Art Gallery</h3>
@@ -87,7 +86,7 @@ function Home() {
                         ))}
                     </div>
                 </div>
-                )}
+                )}  */}
             </div>
             ))}        
         </div>
